8a3fd38e32fe6b3d2be839c1c9f074d311129a65
NayamAmarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-03-26 05:48:23 +0530

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -3,4 +3,4 @@
 sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
 echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
 sudo echo "export QT_STYLE_OVERRIDE=kvantum" >> /etc/environment
-gnome-session-quit
+gnome-session-quit --force

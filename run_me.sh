d02976d861405b5504295a471b6312ef95b04bf6
NayamAmarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-03-26 05:59:35 +0530

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+NC='\033[0m'
+YELLOW='\033[0;33m'
 
 sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
 echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
 sudo echo "export QT_STYLE_OVERRIDE=kvantum" >> /etc/environment
-echo "\n\n"
-echo "${RED}Please logout and login, you'll have the Kvantum app installed. Enjoy theming!"
+echo ""
+echo ""
+echo "#################"
+echo -e "${RED}PLEASE LOGOUT AND LOGIN AGAIN${NC}\nYou'll have the Kvantum app installed. ${YELLOW}Enjoy theming!${NC}"
+echo "#################"

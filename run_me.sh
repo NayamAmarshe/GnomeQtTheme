195b4c76466e3c3f331147d312b3d617189276f1
NayamAmarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-03-26 05:51:52 +0530

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -3,4 +3,4 @@
 sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
 echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
 sudo echo "export QT_STYLE_OVERRIDE=kvantum" >> /etc/environment
-gnome-session-quit --force
+echo "Please logout and login, you'll have the Kvantum app installed. Enjoy theming!"
